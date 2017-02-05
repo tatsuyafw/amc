51ddbcc0ea214580cca90ecc6a7ca31606d68ec7
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2017-02-06 01:15:02 +0900

    Delete a needless line

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import "os"
 const Version string = "0.0.1"
 
 func main() {
-	// cli := &CLI{outStream: os.Stdout, errStream: os.Stderr}
 	cli := NewCLI(os.Stdout, os.Stderr)
 	os.Exit(cli.Run(os.Args[1:]))
 }
