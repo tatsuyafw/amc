5e814520b0896707d63b298420b5bf386af37583
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2017-02-15 23:58:11 +0900

    Add the parameter-groups query to rds

--- aws/rds.go
+++ aws/rds.go
@@ -1,14 +1,26 @@
 package aws
 
+import "github.com/tatsuyafw/amc/util"
+
 type rds struct {
 	query string
 }
 
-func (rds) URL() string {
+var rdsQueries = map[string]string{
+	"parameter-groups": "parameter-groups:",
+}
+
+func (a rds) URL() string {
 	b := "REGION.console.aws.amazon.com/rds/home?region=REGION"
+	if a.query != "" {
+		b += "#" + rdsQueries[a.query]
+	}
 	return url(b)
 }
 
 func (a rds) Validate() bool {
-	return true
+	if a.query == "" {
+		return true
+	}
+	return util.IncludeStr(util.KeysStr(rdsQueries), a.query)
 }
